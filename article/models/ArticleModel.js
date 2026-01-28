0b661148992035505f7945ff6b01dc4fbfc521fd
semih-76 <semihsisman7@gmail.com>
2026-01-28 15:34:25 +0100

    v1.2

---
+++ article/models/ArticleModel.js
@@ -0,0 +1,21 @@
+// Model Articles
+
+const db = require("../../db");
+
+// Récupérer tous les articles
+const getAllArticles = async () => {
+  const [rows] = await db.query("SELECT * FROM articles");
+  return rows;
+};
+
+// Récupéré un article par son ID
+const getArticleById = async (id) => {
+  const [rows] = await db.query("SELECT * FROM articles WHERE id_article = ?", [
+    id,
+  ]);
+  return rows;
+};
+
+// récupérer un article par sa catégorie
+
+module.exports = { getAllArticles, getArticleById };
